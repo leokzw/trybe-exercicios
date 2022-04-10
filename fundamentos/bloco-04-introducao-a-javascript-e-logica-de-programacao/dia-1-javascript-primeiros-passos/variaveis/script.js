e91220c569f76169faa687787841177f9290eb73
Leonardo Konarzewski <leonardo.kzw@gmail.com>
2022-04-10 19:33:52 -0300

    Cria os subdiretórios bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/variaveis com a resolução do exercício de variáveis

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/variaveis/script.js
@@ -0,0 +1,9 @@
+const myName = "Leonardo";
+const birthCity = "Porto Alegre";
+let birthYear = 1994;
+birthYear = 2030;
+birthCity = "Guarani das Missões";
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
