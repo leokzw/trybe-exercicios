2050d5326446c8a0360d7fadd344500c7bb62619
Leonardo Konarzewski <leonardo.kzw@gmail.com>
2022-04-10 22:33:37 -0300

    Cria os subdiretórios operadores-logicos/operador-and com a resolução do exercício

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/operadores-logicos/operador-and/script.js
@@ -0,0 +1,17 @@
+const currentHour = 1;
+let message = "";
+
+if (currentHour >= 22) {
+    message = "Não deveríamos comer nada, é hora de dormir";
+} else if (currentHour >= 18) {
+    message = "Rango da noite, vamos jantar :D";
+} else if (currentHour >= 14) {
+    message = "Vamos fazer um bolo pro café da tarde?";
+} else if (currentHour >= 11) {
+    message = "Hora do almoço!!!";
+} else {
+    message = "Hmmm, cheiro de café recém passado";
+}
+
+console.log(message);
+
