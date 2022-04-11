af603f594c53a6661831a27314041bdee57ff88c
Leonardo Konarzewski <leonardo.kzw@gmail.com>
2022-04-10 23:37:46 -0300

    Cria subdiretório swicth-e-case com a resolução do exercício

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/switch-e-case/script.js
@@ -0,0 +1,15 @@
+let situacao = "reprovado";
+
+switch (situacao) {
+    case "aprovado":
+        console.log("Passou, p*...");
+        break;
+    case "lista":
+        console.log("Hoje não...");
+        break;
+    case "reprovado":
+        console.log("Rodou, se f*...");
+        break;
+    default:
+        console.log("Inválido");
+}
