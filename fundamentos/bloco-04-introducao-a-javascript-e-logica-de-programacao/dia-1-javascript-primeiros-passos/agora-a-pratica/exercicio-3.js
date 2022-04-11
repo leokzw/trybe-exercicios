d0675d4a1429defd518e4b879b95da853849787f
Leonardo Konarzewski <leonardo.kzw@gmail.com>
2022-04-11 00:36:49 -0300

    Adiciona resolução do Exercício 3

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/agora-a-pratica/exercicio-3.js
@@ -0,0 +1,11 @@
+const a = 17;
+const b = 22;
+const c = 38;
+
+if (a > b && a > c) {
+    console.log("a é maior" + " " + "(" + a + ")");
+} else if (b > a && b > c) {
+    console.log("b é maior" + " " + "(" + b + ")");
+} else {
+    console.log("c é maior" + " " + "(" + c + ")");
+}
