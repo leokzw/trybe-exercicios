353ab16414c0449897d3b582c4d05befa3fcce50
Leonardo Konarzewski <leonardo.kzw@gmail.com>
2022-04-16 01:55:11 -0300

    Resolução do exercício 7

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio-1-ao-7.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio-1-ao-7.js
@@ -1,15 +1,11 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-let imparQtd = 0;
+let smallest = numbers[0];
 
- for (let i = 0; i < numbers.length; i += 1) {
-     if (numbers[i] % 2 !== 0) {
-         imparQtd += 1;
+ for (let i = 1; i < numbers.length; i += 1) {
+     if (numbers[i] < smallest) {
+         smallest = numbers[i];
      }
  }
 
- if (imparQtd > 0) {
-    console.log("A quantidade de números ímpares é:" + " " + imparQtd);
- } else {
-     console.log("nenhum valor ímpar encontrado");
- }
+ console.log(smallest);
