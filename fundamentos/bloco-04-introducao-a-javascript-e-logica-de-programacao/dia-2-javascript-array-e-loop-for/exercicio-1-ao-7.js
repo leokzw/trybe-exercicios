ba828c41ecac7c695b0ab1cd16d8a33719785ef9
Leonardo Konarzewski <leonardo.kzw@gmail.com>
2022-04-16 01:32:30 -0300

    Resolução do exercício 5

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio-1-ao-7.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio-1-ao-7.js
@@ -1,16 +1,11 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let sum = 0;
 
- for (let i = 0; i < numbers.length; i += 1) {
-     sum += numbers[i];
+let higher = numbers[0];
+
+ for (let i = 1; i < numbers.length; i += 1) {
+     if (numbers[i] > higher) {
+        higher = numbers[i];
+     }
  }
 
- let average = sum / numbers.length;
-
- console.log(average);
-
- if (average > 20) {
-    console.log("valor maior que 20");
- } else {
-     console.log("valor menor ou igual a 20");
- }
+ console.log(higher);
